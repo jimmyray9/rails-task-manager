2df7575fac4aca601b4ffd04fff19f0af0655c6d
James Gilbert <jimmyray9@gmail.com>
2019-04-17 08:44:57 +0800

    refactored code, added edit function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,43 @@
 class TasksController < ApplicationController
-    def index 
-    end
+  def index
+  	@tasks = Task.all
+  end
 
-    def show
-    end
+  def show
+  	@task = Task.find(params[:id])
+  end
+
+  def edit
+  	@task = Task.find(params[:id])
+  end
+
+  def new
+  	@task = Task.new()
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to tasks_path
+  end
+
+  def update
+  	@task = Task.find(params[:id])
+    @task.update(user_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+  	@task = Task.find(params[:id])
+  	@task.destroy
+
+  	redirect_to tasks_path
+  end
+
+  private
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
+  get "tasks/", to: "tasks#index", as: :tasks
 
-  get "tasks/yo", to: "tasks#show"
+  get    "tasks/new", to: "tasks#new", as: :new
+  post   "tasks", to: "tasks#create"
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "tasks/:id", to: "tasks#show", as: :task
+
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch  "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
